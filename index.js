78c56360ecb862e39d23254eef793ae198365ba7
raj <webmanraj@gmail.com>
2025-09-21 20:20:33 +0530

    added more files

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
+
 const navbar = document.getElementById("nav");
-window.addEventListener("scroll",()=>
-navbar.classList.toggle("sticky",window.scrolly > 0));
+window.addEventListener("scroll", () =>
+  navbar.classList.toggle("sticky", window.scrollY > 0)
+);
 
-const menu =  document.querySelector(".menu");
-const togglemenu = () =>menu.classList.toggle("active");
+const menu = document.querySelector(".menu");
+const toggleMenu = () => menu.classList.toggle("active");
 
-document.querySelector(".menu-btn").addEventListener("click",togglemenu);
-document.querySelector(".close-btn").addEventListener("click",togglemenu);
+document.querySelector(".menu-btn").addEventListener("click", toggleMenu);
+document.querySelector(".close-btn").addEventListener("click", toggleMenu);
+document.querySelectorAll(".menu a")
+  .forEach(link => link.addEventListener("click", toggleMenu));
 
-document.querySelector(".menu a")
-.forEach((link) => link.addEventListener("click",toggleMenu));
+const sr = ScrollReveal({
+  origin: "bottom",
+  distance: "40px",
+  duration: 1000,
+  delay: 400,
+  easing: "ease-in-out",
+});
+
+sr.reveal(".hero-headline", { origin: "left" });
+sr.reveal(".hero-page img", { origin: "right" });
+sr.reveal(".about");
+sr.reveal(".about h1", { delay: 500 });
+sr.reveal(".about p", { delay: 700 });
+sr.reveal(".about-info", { delay: 1000 });
+sr.reveal(".collection h1");
+sr.reveal(".collection-container", { delay: 900 });
+sr.reveal(".review h1");
+sr.reveal(".review-container", { delay: 800 });
+sr.reveal(".callout");
+sr.reveal(".contact");
+
+
